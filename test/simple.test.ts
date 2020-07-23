b1cb1ac02c7b4ba0db534b7f9c2c95dafb240a24
Timo Scholz <scholz.timo99@gmail.com>
2020-07-23 14:47:58 +0200

    Updated tests

--- test/simple.test.ts
+++ test/simple.test.ts
@@ -1,7 +1,6 @@
 import { expect } from 'chai';
 import GenerateTestClassWithoutPromises from './Util/GenerateTestClassWithoutPromises';
 import { DefaultCacheType, Storages } from '../src';
-import GenerateTestClassWithPromises from './Util/GenerateTestClassWithPromises';
 
 describe('Cache counter value', () => {
 
@@ -42,71 +41,4 @@ describe('Cache counter value', () => {
       expect(test3.cachedFunction()).to.equal(0);
     });
   });
-
-  describe('With promises', () => {
-    it ('Should count and compare', async () => {
-
-      const TestClass = GenerateTestClassWithPromises();
-
-      const test1 = new TestClass();
-      const test2 = new TestClass();
-
-
-      // Step 1: Create promise.
-      // Step 2: Request value.
-      const result1 = test1.cachedFunction();
-      // Step 3: Request value.
-      const result2 = test1.cachedFunction();
-
-      const result3 = test2.cachedFunction();
-
-      expect(TestClass.counter).to.equal(0);
-
-      // Step 4: Resolve promise.
-      test1.resolve();
-      const results = await Promise.all([result1, result2]);
-
-      // values from steps 2, 3 should be filled with the resolved value.
-      results.forEach((result) => expect(result).to.equal(0));
-      expect(TestClass.counter).to.equal(1);
-
-      test2.resolve();
-
-      expect(await result3).to.equal(1);
-      expect(TestClass.counter).to.equal(2);
-    });
-
-    it ('Should cache globally', async () => {
-
-      @DefaultCacheType(new Storages.ScopedCacheStorage(), {  })
-      class TestClass extends GenerateTestClassWithPromises() {}
-
-      const test1 = new TestClass();
-      const test2 = new TestClass();
-
-
-      // Step 1: Create promise.
-      // Step 2: Request value.
-      const result1 = test1.cachedFunction();
-      // Step 3: Request value.
-      const result2 = test1.cachedFunction();
-
-      const result3 = test2.cachedFunction();
-
-      expect(TestClass.counter).to.equal(0);
-
-      // Step 4: Resolve promise.
-      test1.resolve();
-      const results = await Promise.all([result1, result2]);
-
-      // values from steps 2, 3 should be filled with the resolved value.
-      results.forEach((result) => expect(result).to.equal(0));
-      expect(TestClass.counter).to.equal(1);
-
-      // test2.resolve();
-
-      expect(await result3).to.equal(0);
-      expect(TestClass.counter).to.equal(1);
-    })
-  })
 });
